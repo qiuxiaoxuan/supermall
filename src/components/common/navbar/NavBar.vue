8b02ccc29b77e49226376f7b97920aa1280d5799
叶丶昀 <321713842@qq.com>
2022-09-29 11:06:24 +0800

    feat: 1、公共导航栏组件_NavBar的封装；2、首页导航栏的创建

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- 因为是公用的，所以使用class不会报错 -->
+  <div class="nav-bar">
+    <!-- 需要对不同的插槽做布局分布，但是slot中没有class属性，所以可以在外边包一层div，再让它们去布局 -->
+    <!-- 使用具名插槽，方便指定不同的替换内容 -->
+    <div class="left"><slot name="left"></slot></div>
+    <div class="center"><slot name="center"></slot></div>
+    <div class="right"><slot name="right"></slot></div>
+  </div>
+</template>
+<script>
+// 封装一个navbar 公用组件 ， navigatorBar 导航栏选项卡
+export default {
+  name: 'NavBar',
+};
+</script>
+<style scoped>
+/* 使用flex布局，左右固定大小，中间占据剩余部分 */
+.nav-bar {
+  /* 表明其内部为flex布局 */
+  display: flex;
+  height: 44px;
+  line-height: 44px;
+  text-align: center;
+  box-shadow: 0 1px 1px rgba(100, 100, 100, 0.1);
+}
+.left,
+.right {
+  width: 60px;
+}
+.center {
+  flex: 1;
+}
+</style>
--- src/views/home/HomeRouter.vue
+++ src/views/home/HomeRouter.vue
@@ -1,8 +1,21 @@
 <template>
-  <h2>主页</h2>
+  <div id="home">
+    <NavBar class="home-nav"><div slot="center">购物街</div></NavBar>
+  </div>
 </template>
 <script>
+import NavBar from 'components/common/navbar/NavBar';
 export default {
   name: 'HomeRouter',
+  components: {
+    NavBar,
+  },
 };
 </script>
+<style scoped>
+.home-nav {
+  /* 设置背景颜色，直接使用css中定义好的变量 */
+  background-color: var(--color-tint);
+  color: #fff;
+}
+</style>
